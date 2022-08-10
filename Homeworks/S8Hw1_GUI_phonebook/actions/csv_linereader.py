dbb594f51d6704176379c082257698d888ab6645
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-08-10 16:42:31 +0300

    Added homework task after seminar 8

---
+++ Homeworks/S8Hw1_GUI_phonebook/actions/csv_linereader.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+def csv_linereader(path: str, filename: str) -> list:
+    filepath = Path(path, filename)
+    with open (filepath, 'r') as file:
+        return file.readlines()
+        
+    
